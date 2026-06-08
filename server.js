6a7b90db21f5b93cac9f63c4bae907281f6ca366
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-07 20:57:40 -0400

    example route with req.params.id

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 // Dependencies
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 const PORT = 3000
+
+
+const projects = ['isitsafeforme', 'hope',]
 
 //MIDDLEWARE
 //this runs in between requests and responses
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
 
 // Routes - INDUCES [Indexes,New, Delete, Update, Create,Edit,Show]
 // app.get('/', (req,res,next)=> {
 //     res.send('Hello World!')
-// })
+// });
 
 // app.get('/about', (req,res) => {
 //     try {
@@ -20,10 +23,15 @@ app.use(express.static(path.join(__dirname,'public')))
 //     } catch (error) {
 //         res.error(error)
 //     }
-// })
+// });
+
+app.get("/projects/:id", (req,res)=> {
+    let id = req.params.id
+    res.send(projects[id])
+});
 
 // Ports
 
 app.listen(PORT,()=>{
     console.log(`Express is listensing on Port ${PORT}`)
-})
+});
