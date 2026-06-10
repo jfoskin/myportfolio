2584685568e42aeee020d0b10f702cca5c355465
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-09 22:01:21 -0400

    included new routes and a middleware function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000
+let previousPage = "None"
 
 
 const projects = ['isitsafeforme', 'hope',]
@@ -10,18 +11,36 @@ const projects = ['isitsafeforme', 'hope',]
 //MIDDLEWARE
 //this runs in between requests and responses
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
+
+app.use((req, res, next)=>{
+    console.log(`hey there the middle ware is running`)
+    console.log(`hey yyyaaaaaaa Previous page ${previousPage} 
+        currentPage ${req.path}`)
+
+    previousPage = req.path
+    next()
+});
 
 // Routes - INDUCES [Indexes,New, Delete, Update, Create,Edit,Show]
-// app.get('/', (req,res,next)=> {
-//     res.send('Hello World!')
-// });
+app.get('/', (req,res)=> {
+    res.send('Hello World!')
+});
+
+app.get('/about', (req,res)=> {
+    res.send('Hello World!')
+});
+
+app.get('/projects', (req,res)=> {
+    // res.send('Hello there!')
+    console.log(req.query)
+});
 
 // app.get('/about', (req,res) => {
 //     try {
 //         res.sendFile(__dirname, 'public','about.html')
 //     } catch (error) {
-//         res.error(error)
+//         res.send(error)
 //     }
 // });
 
@@ -32,6 +51,6 @@ app.get("/projects/:id", (req,res)=> {
 
 // Ports
 
-app.listen(PORT,()=>{
+app.listen(PORT, ()=>{
     console.log(`Express is listensing on Port ${PORT}`)
 });
