fb867ee8e7ddd5347d9803b7545b44229afc8d07
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-11 16:10:32 -0400

    spun up a few test routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Dependencies
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT
 let previousPage = "None"
 
 
