a0a67206acbee71a263440ec1564311708b4a1ce
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-07-27 14:23:26 -0400

    refactor middleware and route handling; improve code readability and structure

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ let previousPage = "None"
 
 const projects = ['isitsafeforme', 'hope',]
 
+
+const adminRoutes = require('./routes/adminRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+
 //MIDDLEWARE
 //this runs in between requests and responses
 
+// app.use(express.json())
 // app.use(express.static(path.join(__dirname,'public')));
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     console.log(`hey there the middle ware is running`)
     console.log(`hey yyyaaaaaaa Previous page ${previousPage} 
         currentPage ${req.path}`)
@@ -23,38 +28,42 @@ app.use((req, res, next)=>{
     next()
 });
 
-app.use('/login',(req,res,next)=>{
-    if(req.headers.x_api_key === 'supersecretkey'){
+app.use('/login', (req, res, next) => {
+    if (req.headers.x_api_key === 'supersecretkey') {
         next()
-    }else{
+    } else {
         res.status(401).json({ error: 'Unauthorized: API Key required' })
     }
 })
 
+app.use('/mgmt', adminRoutes)
+app.use('/projects', projectRoutes)
+
+
 // Routes - INDUCES [Indexes,New, Delete, Update, Create,Edit,Show]
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     console.log(`you did a great job testing with postman`)
 })
 
-app.get('/about', (req,res)=> {
+app.get('/about', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/skills',(req,res)=>{
+app.get('/skills', (req, res) => {
     res.send(`Software engineering skills: JavaScript, TypeScript, React, Html`)
 })
 
 // Example URL: /search?q=javascript&sort=popular
-app.get('/projects', (req,res)=> {
+app.get('/projects', (req, res) => {
     // res.send('Hello there!')
     console.log(req.query)
 });
 
-app.get('/search', (req,res)=>{
+app.get('/search', (req, res) => {
     let category = req.query.cat || 'nothing'
     let size = req.query.size || 'nothing'
 
@@ -71,12 +80,12 @@ app.get('/search', (req,res)=>{
 //     }
 // });
 
-app.get("/projects/:id", (req,res)=> {
+app.get("/projects/:id", (req, res) => {
     let id = req.params.id
     res.send(projects[id])
 });
 
-app.post('/api/users', (req,res)=>{
+app.post('/api/users', (req, res) => {
     const newUsers = req.body
     console.log(`this is the new user ${newUsers}`)
 
@@ -85,6 +94,6 @@ app.post('/api/users', (req,res)=>{
 
 // Ports
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Express is listensing on Port ${PORT}`)
 });
