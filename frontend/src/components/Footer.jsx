58d8be145174607d63df05aca7ed7bcb7ab03b5f
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-07-30 01:06:29 -0400

    feat: update Footer component image and add styling for jolly class

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 
 // import jollyRoger from "../assets/jollyRoger.png"
-import whitejollyRoger from "../assets/whitejolly.png"
+import whitejollyRoger from "../assets/whitejolly.jpg"
 
 export default function Footer() {
   return (
     <div>
         <p>Est: 2026 </p>
-    <img src={whitejollyRoger} alt="Jolly Roger for straw hat pirates from one piece" />
+    <img className="jolly" src={whitejollyRoger} alt="Jolly Roger for straw hat pirates from one piece" />
     </div>
   )
 }
