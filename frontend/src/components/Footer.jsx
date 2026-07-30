2fd42227c3eda0574322d1dc85040aef34fd0ade
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-07-30 01:04:05 -0400

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 
+// import jollyRoger from "../assets/jollyRoger.png"
+import whitejollyRoger from "../assets/whitejolly.png"
 
 export default function Footer() {
   return (
     <div>
         <p>Est: 2026 </p>
-    <img src="../assets/jollyroger.png" alt="Jolly Roger for straw hat pirates from one piece" />
+    <img src={whitejollyRoger} alt="Jolly Roger for straw hat pirates from one piece" />
     </div>
   )
 }
