81cab823d82b04d05714113f40e2a47206978ea6
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-08-10 22:33:19 -0400

    feat: enhance footer styling and add percentage display for improved visual appeal

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,14 @@
 
 // import jollyRoger from "../assets/jollyRoger.png"
-import whitejollyRoger from "../assets/whitejolly.jpg"
+import whitejollyRoger from "../assets/one-piece-jolly-roger.png"
 
 export default function Footer() {
   return (
     <footer className="panel footer-panel">
       <p>Est: 2026</p>
       <img className="jolly" src={whitejollyRoger} alt="Jolly Roger for straw hat pirates from one piece" />
+
+      <p id='percent'>92%</p>
     </footer>
   )
 }
