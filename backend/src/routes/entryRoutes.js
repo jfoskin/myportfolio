007cb180202e4345284b10e2d067d3b7da182dfb
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-08-21 17:26:45 -0400

    feat: implement entry routes for fetching projects with error handling

---
+++ backend/src/routes/entryRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const entryRouter = express.Router();
+const Entry = require('../models/EntryModel');
+
+entryRouter.get('/', async (req, res) => {
+    try {
+        const enries = Entry.find({})
+        res.status(200)
+    } catch (error) {
+        console.log(`Error while trying to get all projects`, error)
+        res.status(400).json({
+            error: `Failed to get all projects`,
+            details: error.message
+        })
+    }
+})
